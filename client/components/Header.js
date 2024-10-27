ad89ea6598adf0f817a939a2de5a3cafade609af
Fantasista766 <proxxx766@gmail.com>
2024-10-27 14:00:51 +0300

    added login button

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,25 @@
-import { MenuItem, Menu } from "semantic-ui-react";
+import { Button, MenuItem, Menu } from "semantic-ui-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
+  const [currentAccount, setCurrentAccount] = useState();
+
+  const hadnleLogInClick = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("У вас нет метамаска");
+    }
+    try {
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Menu style={{ marginTop: "20px" }}>
       <Link href="/">
@@ -15,6 +33,20 @@ const Header = () => {
       <Link href="/show">
         <MenuItem>Посмотреть контакт</MenuItem>
       </Link>
+
+      <MenuItem position="right">
+        {!currentAccount ? (
+          <Button primary onClick={hadnleLogInClick}>
+            Вход
+          </Button>
+        ) : (
+          <Link href="/user">
+            <Button primary onClick={hadnleLogInClick}>
+              {currentAccount}
+            </Button>
+          </Link>
+        )}
+      </MenuItem>
     </Menu>
   );
 };
